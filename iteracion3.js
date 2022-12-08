9d9465106a56de42d25f1a787ed73baa4f9c36fc
ana caballero <anacaballerof@hotmail.com>
2022-12-08 18:35:28 +0100

    iteracion3 de loops

---
+++ iteracion3.js
@@ -0,0 +1,7 @@
+const placesToTravel = 
+['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (let places of placesToTravel){
+    console.log(`Puedo viajar a ${places}`);
+}
+
